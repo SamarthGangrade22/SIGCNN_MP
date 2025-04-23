84f1e13bf94597592812f37d5b980691376c79fd
Suvrat Bajpai <suvratbajpai337@gmail.com>
2025-04-23 14:18:49 +0530

    model updated

--- gnn_model.py
+++ gnn_model.py
@@ -1,19 +1,20 @@
 import torch
-import math
 import torch.nn as nn
 import torch.nn.functional as F
-import random
-
 from torch_geometric.nn import GINConv, JumpingKnowledge, global_mean_pool, SAGEConv
+import math
+
 
 class GIN_Net2(torch.nn.Module):
     def __init__(self, in_len=2000, in_feature=13, gin_in_feature=256, num_layers=1, 
-                hidden=512, use_jk=False, pool_size=3, cnn_hidden=1, train_eps=True, 
-                feature_fusion=None, class_num=7):
+                 hidden=512, use_jk=False, pool_size=3, cnn_hidden=1, train_eps=True, 
+                 feature_fusion=None, class_num=7, info_dim=16, struc_info="EigenValue"):
         super(GIN_Net2, self).__init__()
         self.use_jk = use_jk
         self.train_eps = train_eps
         self.feature_fusion = feature_fusion
+        self.struc_info = struc_info
+        self.info_dim = info_dim
 
         self.conv1d = nn.Conv1d(in_channels=in_feature, out_channels=cnn_hidden, kernel_size=3, padding=0)
         self.bn1 = nn.BatchNorm1d(cnn_hidden)
@@ -22,86 +23,84 @@ class GIN_Net2(torch.nn.Module):
         self.global_avgpool1d = nn.AdaptiveAvgPool1d(1)
         self.fc1 = nn.Linear(math.floor(in_len / pool_size), gin_in_feature)
 
-        self.gin_conv1 = GINConv( 
-            nn.Sequential(
-                nn.Linear(gin_in_feature, hidden),
-                nn.ReLU(),
-                nn.Linear(hidden, hidden),
-                nn.ReLU(),
-                nn.BatchNorm1d(hidden),
-            ), train_eps=self.train_eps
-        )
+        self.gin_conv1 = GINConv(nn.Sequential(
+            nn.Linear(gin_in_feature, hidden),
+            nn.ReLU(),
+            nn.Linear(hidden, hidden),
+            nn.ReLU(),
+            nn.BatchNorm1d(hidden),
+        ), train_eps=self.train_eps)
+
         self.gin_convs = torch.nn.ModuleList()
         for i in range(num_layers - 1):
             self.gin_convs.append(
-                GINConv(
-                    nn.Sequential(
-                        nn.Linear(hidden, hidden),
-                        nn.ReLU(),
-                        nn.Linear(hidden, hidden),
-                        nn.ReLU(),
-                        nn.BatchNorm1d(hidden),
-                    ), train_eps=self.train_eps
-                )
+                GINConv(nn.Sequential(
+                    nn.Linear(hidden, hidden),
+                    nn.ReLU(),
+                    nn.Linear(hidden, hidden),
+                    nn.ReLU(),
+                    nn.BatchNorm1d(hidden),
+                ), train_eps=self.train_eps)
             )
+
         if self.use_jk:
-            mode = 'cat'
-            self.jump = JumpingKnowledge(mode)
-            self.lin1 = nn.Linear(num_layers*hidden, hidden)
+            self.jump = JumpingKnowledge(mode='cat')
+            self.lin1 = nn.Linear(num_layers * hidden, hidden)
         else:
             self.lin1 = nn.Linear(hidden, hidden)
+
         self.lin2 = nn.Linear(hidden, hidden)
-        self.fc2 = nn.Linear(hidden, class_num)
-    
-    def reset_parameters(self):
-        
-        self.conv1d.reset_parameters()
-        self.fc1.reset_parameters()
 
-        self.gin_conv1.reset_parameters()
-        for gin_conv in self.gin_convs:
-            gin_conv.reset_parameters()
-        
-        if self.use_jk:
-            self.jump.reset_parameters()
-        self.lin1.reset_parameters()
-        self.lin2.reset_parameters()
+        fusion_dim = 2 * hidden if self.feature_fusion == 'concat' else hidden
+        self.fc2 = nn.Linear(fusion_dim + info_dim, class_num)
 
-        self.fc2.reset_parameters()
-    
-    def forward(self, x, edge_index, train_edge_id, p=0.5):
+    def forward(self, x, edge_index, train_edge_id, p=0.5, struc_info_values=None):
         x = x.transpose(1, 2)
         x = self.conv1d(x)
         x = self.bn1(x)
         x = self.maxpool1d(x)
         x = x.transpose(1, 2)
         x, _ = self.biGRU(x)
-        x = self.global_avgpool1d(x)
-        x = x.squeeze()
+        x = self.global_avgpool1d(x).squeeze()
         x = self.fc1(x)
 
         x = self.gin_conv1(x, edge_index)
         xs = [x]
         for conv in self.gin_convs:
             x = conv(x, edge_index)
-            xs += [x]
+            xs.append(x)
 
         if self.use_jk:
             x = self.jump(xs)
-        
+
         x = F.relu(self.lin1(x))
         x = F.dropout(x, p=p, training=self.training)
         x = self.lin2(x)
-        # x  = torch.add(x, x_)
 
         node_id = edge_index[:, train_edge_id]
         x1 = x[node_id[0]]
         x2 = x[node_id[1]]
 
         if self.feature_fusion == 'concat':
-            x = torch.cat([x1, x2], dim=1)
+            fused = torch.cat([x1, x2], dim=1)
         else:
-            x = torch.mul(x1, x2)
-        x = self.fc2(x)
+            fused = x1 * x2
 
-        return x
+        # Structural Info Processing
+        processed_info = []
+        for info in struc_info_values:
+            if "Sorted" in self.struc_info:
+                info = sorted(info, reverse=True)
+            if len(info) >= self.info_dim:
+                temp = info[:self.info_dim]
+            else:
+                temp = info + [0] * (self.info_dim - len(info))
+            processed_info.append(temp)
+
+        struc_tensor = torch.tensor(processed_info).float().to(fused.device)
+
+        # Concatenate structure info
+        fused = torch.cat([fused, struc_tensor], dim=1)
+
+        out = self.fc2(fused)
+        return out
